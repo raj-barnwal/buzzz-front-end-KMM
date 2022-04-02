5768ed5fd7497f8c1cdf3564fbef0d48ee89e519
MinkMustela <mink.chauhan@tothenew.com>
2022-04-02 18:04:28 +0530

    react form packege installed

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAisc75Hm77cBYuj0_MzFO_ZTCwK3HxGko",
+  authDomain: "buzzz-dev.firebaseapp.com",
+  projectId: "buzzz-dev",
+  storageBucket: "buzzz-dev.appspot.com",
+  messagingSenderId: "80791830120",
+  appId: "1:80791830120:web:a3009bbaa3aea6ef5185dd",
+  measurementId: "G-DSMJPXF654"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth= getAuth(app);
+export default app;
